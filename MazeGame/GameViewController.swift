dad59c164b3a4b653ec659ed0aa31f9f22156c1e
Leo Peter Stelmaszek <16stelle@kingsford.org>
2018-03-07 15:01:15 -0500

    Organized functions, also attempted to move camera when screen is taped. does not work yet

--- MazeGame/GameViewController.swift
+++ MazeGame/GameViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UIGestureRecognizerDelegate{
     
     var sceneView: SCNView!
     
     var camera = SCNNode()
     var ground = SCNNode()
     var light = SCNNode()
+    var constraint = SCNLookAtConstraint()
+    
     
     //objects to be added to scene
+    var lookAtNode = SCNNode()
     var cylinder1 = SCNNode()
     var sphere1 = SCNNode()
     var sphere2 = SCNNode()
@@ -41,28 +44,28 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
+        initMaterial()
+        initElements()
+        loadMaze()
+        //move()
+    }
+    
+    //is not working yet
+    @IBAction func tapToMove(_ sender: UITapGestureRecognizer) {
+        move()
+    }
+    
+    func move() {
+        print("moving")
+        //lookAtNode.position = SCNVector3(x: 100, y: 0, z: 100)
+        //constraint = SCNLookAtConstraint(target: lookAtNode)
+        self.camera.position = SCNVector3(x: (self.camera.position.x + 5), y: 2.5, z: (self.camera.position.x + 5))
+        print(SCNVector3(x: (self.camera.position.x + 5), y: 2.5, z: (self.camera.position.x + 5)))
         
-        //loading materials with correct color
-        materialGreen.diffuse.contents = UIColor.green
-        materialRed.diffuse.contents = UIColor.red
-        materialBlue.diffuse.contents = UIColor.blue
-        materialPurple.diffuse.contents = UIColor.purple
-        materialCyan.diffuse.contents = UIColor.cyan
-        
-        let cylinderGeometry = SCNCylinder(radius: 0.1, height: 10)
-        cylinderGeometry.materials = [materialGreen]
-        cylinder1 = SCNNode(geometry: cylinderGeometry)
-        cylinder1.position = SCNVector3(x: 0, y: 5, z: 0)
-        
-        let sphereGeometry = SCNSphere(radius: 1.5)
-        sphereGeometry.materials = [materialPurple]
-        sphere1 = SCNNode(geometry: sphereGeometry)
-        sphere1.position = SCNVector3(x: -5, y: 1.5, z: 0)
-        sphere2 = SCNNode(geometry: sphereGeometry)
-        sphere2.position = SCNVector3(x: 0, y: 1.5, z: -5)/**/
-        
-        sphere1.addChildNode(camera)
-        
+        //lookAtNode.position = SCNVector3(x: lookAtNode.position.x + 5, y: 2.5, z: 0)
+    }
+    
+    func loadMaze() {
         let borderWallxGeometry = SCNBox(width: 100, height: 5, length: 1, chamferRadius: 0)
         borderWallxGeometry.materials = [materialRed]
         borderWallx = SCNNode(geometry: borderWallxGeometry)
@@ -73,47 +76,35 @@ class GameViewController: UIViewController {
         borderWallz = SCNNode(geometry: borderWallzGeometry)
         borderWallz.position = SCNVector3(x: -0.5, y: 1.5, z: 50)
         
-        sceneView.allowsCameraControl = true
-        sceneView.scene?.rootNode.addChildNode(self.camera)
-        sceneView.scene?.rootNode.addChildNode(ground)
-        sceneView.scene?.rootNode.addChildNode(light)
-        sceneView.scene?.rootNode.addChildNode(cylinder1)
-        sceneView.scene?.rootNode.addChildNode(sphere1)
-        
-        
-        
         sceneView.scene?.rootNode.addChildNode(borderWallx)
         sceneView.scene?.rootNode.addChildNode(borderWallz)
         
-        loadMaze()
-        
-    }
-    
-    
-    func loadMaze() {
         let wallxGeometry = SCNBox(width: 5, height: 5, length: 1, chamferRadius: 0)
         wallxGeometry.materials = [materialRed]
         wallx += [SCNNode(geometry: wallxGeometry)]
         wallx[0].position = SCNVector3(x: 7.5, y: 1.5, z: 10)
         wallx.append(SCNNode(geometry: wallxGeometry))
-        wallx[1].position = SCNVector3(x: 9.5, y: 1.5, z: 12)
+        wallx[1].position = SCNVector3(x: 12.5, y: 1.5, z: 15)
         
         let wallzGeometry = SCNBox(width: 1, height: 5, length: 5, chamferRadius: 0)
         wallzGeometry.materials = [materialCyan]
         wallz += [SCNNode(geometry: wallzGeometry)]
         wallz[0].position = SCNVector3(x: 10, y: 1.5, z: 7.5)
         
-        
-        sceneView.scene?.rootNode.addChildNode(wallx[0])
-        sceneView.scene?.rootNode.addChildNode(wallx[1])
-        sceneView.scene?.rootNode.addChildNode(wallz[0])
+        for child in wallx {
+            //print(child.position)
+            sceneView.scene?.rootNode.addChildNode(child)
+        }
+        for child in wallz {
+            //print(child.position)
+            sceneView.scene?.rootNode.addChildNode(child)
+        }
     }
     
     func initView() {
         sceneView = SCNView(frame: self.view.frame)
         sceneView.scene = SCNScene()
         self.view.addSubview(sceneView)
-        
         
         let groundGeometry = SCNFloor()
         groundGeometry.reflectivity = 0
@@ -122,14 +113,16 @@ class GameViewController: UIViewController {
         groundGeometry.materials = [groundMaterial]
         ground = SCNNode(geometry: groundGeometry)
         
+        lookAtNode.position = SCNVector3(x: 100, y: 0, z: 100)
+        
         let camera = SCNCamera()
-        camera.zFar = 10000
+        //camera.zFar = 10000
         self.camera = SCNNode()
         self.camera.camera = camera
-        self.camera.position = SCNVector3(x: -30, y: 20, z: -30)
-        let constraint = SCNLookAtConstraint(target: ground)
+        constraint = SCNLookAtConstraint(target: lookAtNode)
         constraint.isGimbalLockEnabled = true
         self.camera.constraints = [constraint]
+        self.camera.position = SCNVector3(x: 0, y: 5, z: 0)
         
         let ambientLight = SCNLight()
         ambientLight.color = UIColor.darkGray
@@ -147,6 +140,36 @@ class GameViewController: UIViewController {
         light.light = spotLight
         light.position = SCNVector3(x: -25, y: 15, z: -25)
         light.constraints = [constraint]
+    }
+    
+    func initMaterial() {
+        //loading materials with correct color
+        materialGreen.diffuse.contents = UIColor.green
+        materialRed.diffuse.contents = UIColor.red
+        materialBlue.diffuse.contents = UIColor.blue
+        materialPurple.diffuse.contents = UIColor.purple
+        materialCyan.diffuse.contents = UIColor.cyan
+    }
+    
+    func initElements() {
+        let cylinderGeometry = SCNCylinder(radius: 0.1, height: 10)
+        cylinderGeometry.materials = [materialGreen]
+        cylinder1 = SCNNode(geometry: cylinderGeometry)
+        cylinder1.position = SCNVector3(x: 0, y: 5, z: 0)
+        
+        let sphereGeometry = SCNSphere(radius: 1.5)
+        sphereGeometry.materials = [materialPurple]
+        sphere1 = SCNNode(geometry: sphereGeometry)
+        sphere1.position = SCNVector3(x: -5, y: 1.5, z: 0)
+        sphere2 = SCNNode(geometry: sphereGeometry)
+        sphere2.position = SCNVector3(x: 0, y: 1.5, z: -5)/**/
+        
+        sceneView.allowsCameraControl = true
+        sceneView.scene?.rootNode.addChildNode(self.camera)
+        sceneView.scene?.rootNode.addChildNode(ground)
+        sceneView.scene?.rootNode.addChildNode(light)
+        sceneView.scene?.rootNode.addChildNode(cylinder1)
+        sceneView.scene?.rootNode.addChildNode(sphere1)
     }
     
     override var shouldAutorotate: Bool {
